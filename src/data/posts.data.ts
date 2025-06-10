e126b8e85cc40d69acea7f0e0857752e375ded62
ZvonimirSun <hi@iszy.xyz>
2025-06-10 14:37:54 +0800

    feat: 文章按时间进行排序

--- src/data/posts.data.ts
+++ src/data/posts.data.ts
@@ -3,4 +3,11 @@ import { createContentLoader } from 'vitepress'
 declare const data: any[]
 export { data }
 
-export default createContentLoader('_posts/*.md' /* options */)
+export default createContentLoader('_posts/*.md', {
+  transform(rawData) {
+    // 文章按时间排序
+    return rawData.sort((a, b) => {
+      return (a.frontmatter.date ?? new Date()) > (b.frontmatter.date ?? new Date()) ? -1 : 1
+    })
+  },
+})
