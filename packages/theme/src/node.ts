3ee1c05923c97d1e43fdfac077fe00f95d2261b0
ZvonimirSun <hi@iszy.xyz>
2025-06-18 21:57:16 +0800

    feat: 调整.gitignore以及文件目录结构

--- packages/theme/src/node.ts
+++ packages/theme/src/node.ts
@@ -1,4 +1,4 @@
-export * from './utils/paths'
-export * from './utils/posts'
-export * from './utils/theme'
+export * from './utils/node/paths'
+export * from './utils/node/posts'
+export * from './utils/node/theme'
 export type * from '@zvonimirsun/vitepress-theme/types'
